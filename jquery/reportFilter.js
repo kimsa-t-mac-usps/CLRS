5b2c16f2d33be34b71d8741bea80cd0e78c81a5b
k6gvn0 <barbara.k.severin@usps.gov>
2024-08-05 13:18:30 -0400

    update select division

--- jquery/reportFilter.js
+++ jquery/reportFilter.js
@@ -1,9 +1,17 @@
 $(document).ready(function(){
     let url="report.full.cfm?";
+
     $('#Select_DIST_PERF_CLUSTER_CODE').on('change',function() {
         let selectedVal = $('#Select_DIST_PERF_CLUSTER_CODE').val();
         url = url + "SelectedPC=" + selectedVal;
         let encoded = encodeURI(url);
         location.href = encoded;
-    })
+    });
+
+    $('#Select_DIVISION_CODE').on('change',function() {
+        let selectedVal = $('#Select_DIVISION_CODE').val();
+        url = url + "SelectedPC=" + selectedVal;
+        let encoded = encodeURI(url);
+        location.href = encoded;
+    });
 });
